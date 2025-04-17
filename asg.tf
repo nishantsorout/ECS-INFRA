58295e25f4eb3af16eac55f7a9009928dcb03be7
NishantSorout-BBT <nishant.sorout@bellblazetech.com>
2025-04-17 12:27:21 +0530

    Add files via upload

--- asg.tf
+++ asg.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_group" "ecs_asg" {
   name = "ktt-ecs-asg"
-  min_size             = 1
-  desired_capacity     = 1
+  min_size             = 0
+  desired_capacity     = 1  # make it 0 while scheduling the servers
   max_size             = 5
   
   vpc_zone_identifier = var.subnet_ids
@@ -17,7 +17,7 @@ resource "aws_autoscaling_group" "ecs_asg" {
 }
 
 /*
-# Start EC2 Mon–Fri at 10 AM IST (04:30 UTC)
+# Start EC2 Mon–Fri at 10:30 AM IST (05:00 UTC)
 resource "aws_autoscaling_schedule" "start_instance_in_morning" {
   scheduled_action_name  = "start-ec2-morning"
   autoscaling_group_name = aws_autoscaling_group.ecs_asg.name
@@ -27,14 +27,14 @@ resource "aws_autoscaling_schedule" "start_instance_in_morning" {
   recurrence           = "30 4 * * 1-5"  # Mon–Fri only
 }
 
-# Stop EC2 Mon-Fri at 7 PM IST (13:30 UTC)
+# Stop EC2 Mon-Fri at 7:30 PM IST (14:00 UTC)
 resource "aws_autoscaling_schedule" "stop_instance_in_evening" {
   scheduled_action_name  = "stop-ec2-evening"
   autoscaling_group_name = aws_autoscaling_group.ecs_asg.name
   min_size             = 0
   desired_capacity     = 0
   max_size             = 5
-  recurrence              = "30 13 * * *"  # 13:30 UTC daily
+  recurrence              = "00 14 * * *"  # 14:00 UTC daily
 }
 
 
